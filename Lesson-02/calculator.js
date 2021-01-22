9616295f028208daea3f65bee307c16bfa663ab8
DAN POWELL <danpowell@DANs-MacBook-Air.local>
2021-01-22 14:16:58 -0800

    Adjusted calculator

--- Lesson-02/calculator.js
+++ Lesson-02/calculator.js
@@ -2,13 +2,15 @@ let firstNumber = 100;
 let secondNumber = 40;
 let resultAddition = firstNumber + secondNumber;
 let resultSubtraction = firstNumber - secondNumber;
-let resultMultiplication = firstNumber * secondNumber;
-let resultDivision = firstNumber / secondNumber;
-console.log ('firstNumber = 100');
-console.log('secondNumber = 40');
+let resultMultiplication = secondNumber * firstNumber ;
+let resultDivision = secondNumber /  firstNumber;
 console.log('Operations in order are addition, subtraction, multiplication, then division');
+console.log ('100+40 =')
 console.log (resultAddition);
+console.log('100-40 =')
 console.log (resultSubtraction);
+console.log('40+100 =')
 console.log (resultMultiplication);
+console.log('40 / 100 = ')
 console.log (resultDivision);
 console.log('Operations Complete');
