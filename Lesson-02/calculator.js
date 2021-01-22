47e4c4afff7a55ff0eab614a4a499e04ff6a9ce0
DAN POWELL <danpowell@DANs-MacBook-Air.local>
2021-01-22 14:51:50 -0800

    Added spacing

--- Lesson-02/calculator.js
+++ Lesson-02/calculator.js
@@ -5,11 +5,11 @@ let resultSubtraction = firstNumber - secondNumber;
 let resultMultiplication = secondNumber * firstNumber ;
 let resultDivision = secondNumber /  firstNumber;
 console.log('Operations in order are addition, subtraction, multiplication, then division');
-console.log ('100+40 =')
+console.log ('100 + 40 =')
 console.log (resultAddition);
-console.log('100-40 =')
+console.log('100 - 40 =')
 console.log (resultSubtraction);
-console.log('40+100 =')
+console.log('40 * 100 =')
 console.log (resultMultiplication);
 console.log('40 / 100 = ')
 console.log (resultDivision);
