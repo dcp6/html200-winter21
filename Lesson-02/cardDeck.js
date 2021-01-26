edb7ffb0f66b82b454522a98e7f1e39e940fb0b1
DAN POWELL <danpowell@DANs-MacBook-Air.local>
2021-01-26 12:29:39 -0800

    Added deck of cards

---
+++ Lesson-02/cardDeck.js
@@ -0,0 +1,108 @@
+  let name = '2';
+  let value = 'Hearts'
+  let nameCounter = 0;
+  let suitCounter = 1;
+
+
+for (i = 2; i < 11; i ++){
+  console.log(`${i} of ${value}`);
+  nameCounter ++;
+}
+if (nameCounter = 9){
+  name = 'Jack';
+  console.log(`${name} of ${value}`);
+  
+}
+nameCounter ++;
+if (nameCounter = 10){
+  name = 'Queen';
+  console.log(`${name} of ${value}`);
+}
+nameCounter ++;
+if (nameCounter = 11){
+  name = 'King';
+  console.log(`${name} of ${value}`);
+}
+nameCounter ++;
+if (nameCounter = 12){
+  name = 'Ace';
+  console.log(`${name} of ${value}`);
+}
+value = 'Spades';
+
+for (i = 2; i < 11; i ++){
+  console.log(`${i} of ${value}`);
+  nameCounter ++;
+}
+if (nameCounter = 9){
+  name = 'Jack';
+  console.log(`${name} of ${value}`);
+  
+}
+nameCounter ++;
+if (nameCounter = 10){
+  name = 'Queen';
+  console.log(`${name} of ${value}`);
+}
+nameCounter ++;
+if (nameCounter = 11){
+  name = 'King';
+  console.log(`${name} of ${value}`);
+}
+nameCounter ++;
+if (nameCounter = 12){
+  name = 'Ace';
+  console.log(`${name} of ${value}`);
+}
+value = 'Diamonds';
+for (i = 2; i < 11; i ++){
+  console.log(`${i} of ${value}`);
+  nameCounter ++;
+}
+if (nameCounter = 9){
+  name = 'Jack';
+  console.log(`${name} of ${value}`);
+  
+}
+nameCounter ++;
+if (nameCounter = 10){
+  name = 'Queen';
+  console.log(`${name} of ${value}`);
+}
+nameCounter ++;
+if (nameCounter = 11){
+  name = 'King';
+  console.log(`${name} of ${value}`);
+}
+nameCounter ++;
+if (nameCounter = 12){
+  name = 'Ace';
+  console.log(`${name} of ${value}`);
+}
+value = 'Clubs';
+
+for (i = 2; i < 11; i ++){
+  console.log(`${i} of ${value}`);
+  nameCounter ++;
+}
+if (nameCounter = 9){
+  name = 'Jack';
+  console.log(`${name} of ${value}`);
+  
+}
+nameCounter ++;
+if (nameCounter = 10){
+  name = 'Queen';
+  console.log(`${name} of ${value}`);
+}
+nameCounter ++;
+if (nameCounter = 11){
+  name = 'King';
+  console.log(`${name} of ${value}`);
+}
+nameCounter ++;
+if (nameCounter = 12){
+  name = 'Ace';
+  console.log(`${name} of ${value}`);
+}
+console.log('Operation Complete');
