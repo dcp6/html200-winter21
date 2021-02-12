96f01c74cda1f141b2aa2ebc6f60cfcac2753ad5
DAN POWELL <danpowell@DANs-MacBook-Air.local>
2021-02-11 16:16:33 -0800

    Finalized build

---
+++ Lesson-04/myscript.js
@@ -0,0 +1,13 @@
+function recieveInput() {
+	let userInput = prompt('Enter a number to square');
+	userInput = userInput * userInput;
+	alert(userInput);
+}
+//let squaredInput = ' ';
+//
+//function squareCalc(squareInput) {
+//  squaredValue = squareInput**2;
+//  return squaredValue;
+//}
+//
+//squareCalc(squaredInput);
