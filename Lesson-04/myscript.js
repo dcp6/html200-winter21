b3be19dd2b507c63f7ed411e16df939ce6fd59ff
DAN POWELL <danpowell@DANs-MacBook-Air.local>
2021-02-11 16:30:30 -0800

    Added bonus

--- Lesson-04/myscript.js
+++ Lesson-04/myscript.js
@@ -11,3 +11,9 @@ function recieveInput() {
 //}
 //
 //squareCalc(squaredInput);
+function recieveInput2() {
+	let userInput = prompt('Enter the first number');
+    let userInputTwo = prompt('Enter the second number');
+	outputValue = userInput * userInputTwo;
+	alert(outputValue);
+}
---
+++ Lesson-04/myscript2.js
@@ -0,0 +1,6 @@
+function recieveInput2() {
+	let userInput = prompt('Enter the first number');
+    let userInputTwo = prompt('Enter the second number');
+	outputValue = userInput * userInputTwo;
+	alert(outputValue);
+}
