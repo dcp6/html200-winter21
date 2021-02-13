43886eeea63d4dbaa470abaf05562b51fc03873f
DAN POWELL <danpowell@DANs-MacBook-Air.local>
2021-02-12 21:04:44 -0800

    Finalized bank account

--- Lesson-05/myScripts.js
+++ Lesson-05/myScripts.js
@@ -1,4 +1,4 @@
-total = 1000;
+total = 5000;
 pressedKey ='';
 function bankApplication(){
     pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance')
@@ -6,24 +6,36 @@ function bankApplication(){
     do {
     switch (pressedKey) {
         case 'Q':
-        alert('You pressed Q');
+     pressedKey = 'Q';
+        
         break;
         case 'W':
          total = total- Number(prompt('Enter the amount you wish to withdraw'));
          alert('Your new total is ' + '$' + total);
         pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance');
+         pressedKey = pressedKey.toUpperCase();
         break;
 
         case 'D':
         total = total + Number(prompt('Enter the amount you wish to deposit'));
         alert('Your new total is ' + '$' + total);
-        break;
-        default:
-        alert('Invalid input');
+        pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance');
+        pressedKey = pressedKey.toUpperCase();
         
+        break;
+        case 'B':
+       alert('Your total is ' + '$' + total);
+       pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance');
+        pressedKey = pressedKey.toUpperCase();
+       break;
+      default:
+        
+        alert('Invalid input');
+        pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance');
+        pressedKey = pressedKey.toUpperCase();
     }
     }
-    while (pressedKey!==Q);   
+    while (pressedKey !='Q');   
     
     
     
