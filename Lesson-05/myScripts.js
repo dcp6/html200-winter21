0ff476d3c3570e3044573a886021d1d4f62bc44e
DAN POWELL <danpowell@DANs-MacBook-Air.local>
2021-02-15 14:41:43 -0800

    Added 300 or less confirm box

--- Lesson-05/myScripts.js
+++ Lesson-05/myScripts.js
@@ -18,26 +18,31 @@ function bankApplication() {
                     pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance');
                     pressedKey = pressedKey.toUpperCase();
 
-                } else if (totalLeft < 300 && totalLeft > 0) {
+                }//withdraw amount bracket//
+                else if (totalLeft < 300 && totalLeft >= 0) {
+                    let confirmBox = confirm('Please confirm you wish to go below a balance of $300.');
+                    if (confirmBox = true){
                     total = total - withdrawlAmount;
-                    alert('Your total is below $300:' + '$' + total);
+                    alert('Your total is:' + ' $' + total);
                     pressedKey = pressedKey.toUpperCase();
+                    }//confirm box closing bracket//
+                    else {
+                    alert('You chose not to withdraw any money');
+                   
+                    }//confirm box second closing bracket//
 
                 } else {
                     alert('You cannot withdraw any more money.');
-
                 }
-                pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance');
-                pressedKey = pressedKey.toUpperCase();
-
-                break;
+                  pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance');
+                    pressedKey = pressedKey.toUpperCase();
+                 break;
 
             case 'D':
                 depositAmount = Number(prompt('Enter the amount you wish to deposit'));
                 if (depositAmount < 50000) {
                     total = total + depositAmount;
                     alert('Your new total is ' + '$' + total);
-                    pressedKey = prompt('Press Q to quit, W to withdraw, D to deposit, or B to see your balance');
                     pressedKey = pressedKey.toUpperCase();
                 } else {
                     alert('You cannot deposit more than $50,000.');
